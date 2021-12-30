230a806f9d468c94a1277d8157642cc2d026e30e
trevor <me@trevorbehnke.com>
2021-12-29 20:32:34 -0800

    merge transitions

--- menu/menu.js
+++ menu/menu.js
@@ -1,13 +1,13 @@
+// select the svg container first
 const svg = d3
   .select(".canvas")
   .append("svg")
   .attr("width", 600)
   .attr("height", 600);
 
+// create margins & dimensions
 const margin = { top: 20, right: 20, bottom: 100, left: 100 };
-
 const graphWidth = 600 - margin.left - margin.right;
-
 const graphHeight = 600 - margin.top - margin.bottom;
 
 const graph = svg
@@ -16,84 +16,96 @@ const graph = svg
   .attr("height", graphHeight)
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+// create axes groups
 const xAxisGroup = graph
   .append("g")
   .attr("transform", `translate(0, ${graphHeight})`);
+
+xAxisGroup
+  .selectAll("text")
+  .attr("fill", "teal")
+  .attr("transform", "rotate(-40)")
+  .attr("text-anchor", "end");
 
 const yAxisGroup = graph.append("g");
 
 const y = d3.scaleLinear().range([graphHeight, 0]);
 
-const x = d3.scaleBand().range([0, 500]).paddingInner(0.2).paddingOuter(0.2);
+const x = d3
+  .scaleBand()
+  .range([0, graphWidth])
+  .paddingInner(0.2)
+  .paddingOuter(0.2);
 
-// creat the axes
+// create & call axes
 const xAxis = d3.axisBottom(x);
 const yAxis = d3
   .axisLeft(y)
-  .ticks(5)
+  .ticks(3)
   .tickFormat((d) => d + " orders");
 
-// update x axis text
-xAxisGroup
-  .selectAll("text")
-  .attr("fill", "white")
-  .attr("text-anchor", "end")
-  .attr("transform", "rotate(-45)");
+const t = d3.transition().duration(500);
 
-// update function
+// the update function
 const update = (data) => {
-  // update scales
-  y.domain([0, d3.max(data, (d) => d.orders)]);
-  x.domain(data.map((d) => d.name));
+  // join the data to circs
+  const rects = graph.selectAll("rect").data(data);
 
-  // join new data with old elements
-  const rects = graph.selectAll("rects").data(data);
-
-  // remove unwanted elements
+  // remove unwanted rects
   rects.exit().remove();
 
-  // update old elements
+  // update the domains
+  y.domain([0, d3.max(data, (d) => d.orders)]);
+  x.domain(data.map((item) => item.name));
+
+  // add attrs to rects already in the DOM
   rects
     .attr("width", x.bandwidth)
-    .attr("height", (d) => graphHeight - y(d.orders))
-    .attr("x", (d) => x(d.name))
-    .attr("y", (d) => y(d.orders))
-    .attr("fill", "teal");
+    .attr("fill", "teal")
+    .attr("x", (d) => x(d.name));
+  // .transition(t)
+  //   .attr("height", d => graphHeight - y(d.orders))
+  //   .attr('y', d => y(d.orders));
 
   // append the enter selection to the DOM
   rects
     .enter()
     .append("rect")
     .attr("width", x.bandwidth)
-    .attr("height", (d) => graphHeight - y(d.orders))
+    .attr("height", (d) => 0)
+    .attr("fill", "teal")
     .attr("x", (d) => x(d.name))
-    .attr("y", (d) => y(d.orders))
-    .attr("fill", "teal");
+    .attr("y", (d) => graphHeight)
+    .merge(rects)
+    .transition(t)
+    .attr("height", (d) => graphHeight - y(d.orders))
+    .attr("y", (d) => y(d.orders));
 
-  // call axes
   xAxisGroup.call(xAxis);
   yAxisGroup.call(yAxis);
 };
 
-let data = [];
+var data = [];
 
 db.collection("dishes").onSnapshot((res) => {
   res.docChanges().forEach((change) => {
     const doc = { ...change.doc.data(), id: change.doc.id };
+
     switch (change.type) {
       case "added":
         data.push(doc);
         break;
       case "modified":
-        const index = data.findIndex((item) => item.id === doc.id);
+        const index = data.findIndex((item) => item.id == doc.id);
         data[index] = doc;
         break;
       case "removed":
-        data = data.filter((item) => item.id != doc.id);
+        data = data.filter((item) => item.id !== doc.id);
         break;
       default:
         break;
     }
   });
+
   update(data);
 });
